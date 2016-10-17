87ad1ec79882173ce9353ed17c5e302926e017d6
iaink <iainkinnish@googlemail.com>
2016-10-17 15:26:17 +0000

    Created a Link class and added some new gems.

--- Gemfile
+++ Gemfile
@@ -2,3 +2,5 @@ source "https://rubygems.org"
 
 gem "data_mapper"
 gem "dm-postgres-adapter"
+gem "capybara"
+gem "rspec"
---
+++ spec/viewing_links_spec.rb
@@ -0,0 +1,14 @@
+feature 'Viewing Links' do
+  
+  scenario 'I can view links on the link page' do
+    Link.create(url: 'http://www.google.com', title: 'Google')
+    
+    visit '/links'
+    
+    expect(page.status_code).to eq 200
+    
+    within 'ul#links' do
+      expect(page).to have_content 'Google'
+    end
+  end
+end
