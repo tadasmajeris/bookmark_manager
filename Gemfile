b6f5e80184a3c9824ec231c9817b13ce5916ffc0
Stephen Gregory <stejgregory@gmail.com>
2016-10-18 14:54:41 +0100

    Compleated 12, Database Cleaner added

--- Gemfile
+++ Gemfile
@@ -2,11 +2,10 @@ source "https://rubygems.org"
 
 ruby '2.3.1'
 
+gem 'database_cleaner'
 gem 'data_mapper'
 gem 'dm-postgres-adapter'
 gem 'rspec'
 gem 'capybara'
 gem 'rspec-sinatra'
 gem 'sinatra'
-
-end
--- spec/features/viewing_links_spec.rb
+++ spec/features/viewing_links_spec.rb
@@ -4,6 +4,7 @@ feature 'Viewing links' do
 
   scenario 'I can see existing links on the links page' do
     # We can use `.create`, which we used in irb to make a Student, within our test!
+    Link.create(url: 'http://www.makersacademy.co.uk/', title: 'Makers Academy')
     visit '/links'
 
     # this is a *temporary* sanity check - to make sure we
