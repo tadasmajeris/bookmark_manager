937b1195714e22bc11cd410d0317ab10758f000f
crispinandrews <crispin.andrews@gmail.com>
2016-10-18 10:24:16 +0100

    Stage 10 complete. Added app.rb, added views, tests Green

---
+++ app/app.rb
@@ -0,0 +1,14 @@
+require 'sinatra/base'
+require_relative 'models/link'
+
+class BookmarkManager < Sinatra::Base
+
+  get '/links' do
+    @links = Link.all
+    erb(:links)
+  end
+
+
+  # start the server if ruby file executed directly
+  run! if app_file == $0
+end
--- app/models/link.rb
+++ app/models/link.rb
@@ -2,7 +2,7 @@ require 'data_mapper'
 require 'dm-postgres-adapter'
 
 class Link
-  include DataMapper::Resource 
+  include DataMapper::Resource
   property :id, Serial
   property :title, String
   property :url, String
