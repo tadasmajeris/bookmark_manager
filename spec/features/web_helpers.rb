fbfe8d610b406bc64571e891cc729457c0e42fe3
crispinandrews <crispin.andrews@gmail.com>
2016-10-18 17:03:58 +0100

    Stage 19 done. Password confirmations added

--- spec/features/web_helpers.rb
+++ spec/features/web_helpers.rb
@@ -18,10 +18,13 @@ def add_google_link
   click_button 'Add link'
 end
 
-def sign_up
+def sign_up(email: 'alice@example.com',
+            password: 'oranges!',
+            password_confirmation: 'oranges!')
   visit '/users/new'
   expect(page.status_code).to eq(200)
-  fill_in :email,    with: 'alice@example.com'
-  fill_in :password, with: 'oranges!'
+  fill_in :email,    with: email
+  fill_in :password, with: password
+  fill_in :password_confirmation, with: password_confirmation
   click_button 'Sign up'
 end
