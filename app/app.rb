591fc2789e9fd6eb9cac99cbe7de48c2c329ad8f
crispinandrews <crispin.andrews@gmail.com>
2016-10-18 12:15:52 +0100

    Deploy to heroku

--- app/app.rb
+++ app/app.rb
@@ -5,6 +5,10 @@ require_relative 'models/link'
 
 class BookmarkManager < Sinatra::Base
 
+  get '/' do
+    redirect '/links'
+  end
+
   get '/links' do
     @links = Link.all
     erb(:'links/index')
