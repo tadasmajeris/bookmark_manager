9c1a9013a5c6080da2bbfd35bbe5f39bb0c23331
Tadas Majeris <tadas.majeris@gmail.com>
2016-10-18 09:12:09 +0100

    Viewing links test

--- spec/viewing_links_spec.rb
+++ spec/viewing_links_spec.rb
@@ -1,12 +1,14 @@
+require "spec_helper"
+
 feature 'Viewing Links' do
-  
+
   scenario 'I can view links on the link page' do
     Link.create(url: 'http://www.google.com', title: 'Google')
-    
+
     visit '/links'
-    
+
     expect(page.status_code).to eq 200
-    
+
     within 'ul#links' do
       expect(page).to have_content 'Google'
     end
