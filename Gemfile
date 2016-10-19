e24e70f3b4fd6857e571454c225253204713a093
James <bakerje@mac.com>
2016-10-19 17:22:02 +0100

    Various changes to complete step 18

--- Gemfile
+++ Gemfile
@@ -6,6 +6,7 @@ ruby '2.3.1'
 gem 'sinatra'
 gem 'data_mapper'
 gem 'dm-postgres-adapter'
+gem 'bcrypt'
 
 group :test do
   gem 'database_cleaner'
--- app/app.rb
+++ app/app.rb
@@ -4,6 +4,9 @@ require_relative 'data_mapper_setup'
 ENV["RACK_ENV"] ||= "development"
 
 class BookmarkManager < Sinatra::Base
+  enable :sessions
+  set :session_secret, 'super secret'
+
   get '/links' do
     @links = Link.all
     erb :'/links/index'
@@ -26,6 +29,24 @@ class BookmarkManager < Sinatra::Base
     @links = tag ? tag.links : []
     erb :'links/index'
   end
+
+  get '/users/new' do
+    erb :'users/new'
+  end
+
+  post '/users' do
+    user = User.create(email: params[:email],
+                      password: params[:password])
+    session[:user_id] = user.id
+    redirect to('/links')
+  end
+
+  helpers do
+    def current_user
+      @current_user ||= User.get(session[:user_id])
+    end
+  end
+
 
   # start the server if ruby file executed directly
   run! if app_file == $0
