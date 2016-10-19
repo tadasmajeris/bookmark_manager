655c2bb2ebd8e335c9cf35fa715eb47565f78899
Stephen Gregory <stejgregory@gmail.com>
2016-10-19 11:32:05 +0100

    Tag model complete, all passing

--- spec/features/viewing_links_spec.rb
+++ spec/features/viewing_links_spec.rb
@@ -2,9 +2,15 @@ require 'spec_helper'
 
 feature 'Viewing links' do
 
+  before(:each) do
+    Link.create(url: 'http://www.makersacademy.com', title: 'Makers Academy', tags: [Tag.first_or_create(name: 'education')])
+    Link.create(url: 'http://www.google.com', title: 'Google', tags: [Tag.first_or_create(name: 'search')])
+    Link.create(url: 'http://www.zombo.com', title: 'This is Zombocom', tags: [Tag.first_or_create(name: 'bubbles')])
+    Link.create(url: 'http://www.bubble-bobble.com', title: 'Bubble Bobble', tags: [Tag.first_or_create(name: 'bubbles')])
+  end
+
   scenario 'I can see existing links on the links page' do
-    # We can use `.create`, which we used in irb to make a Student, within our test!
-    Link.create(url: 'http://www.makersacademy.co.uk/', title: 'Makers Academy')
+
     visit '/links'
 
     # this is a *temporary* sanity check - to make sure we
@@ -13,6 +19,20 @@ feature 'Viewing links' do
 
     within 'ul#links' do
       expect(page).to have_content('Makers Academy')
+    end
+  end
+
+
+
+  scenario 'I can filter links by tag' do
+    visit '/tags/bubbles'
+
+    expect(page.status_code).to eq(200)
+    within 'ul#links' do
+      expect(page).not_to have_content('Makers Academy')
+      expect(page).not_to have_content('Code.org')
+      expect(page).to have_content('This is Zombocom')
+      expect(page).to have_content('Bubble Bobble')
     end
   end
 end
